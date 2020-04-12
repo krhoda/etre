47c02fb8da63971031d058b9730154d4645519c7
K Rhoda <kelseydrhoda@gmail.com>
2020-04-12 14:13:07 -0600

    Finished codon table

--- codon/src/lib.rs
+++ codon/src/lib.rs
@@ -31,12 +31,19 @@ impl Codon {
     }
 
     // NOTE: Ignoring the AUG => START command. Will use seperate function for this.
-    pub fn to_amino_encoding(&self, first: bool) -> Amino {
+    pub fn to_amino_encoding(&self) -> Amino {
         let (fst, snd, thd) = &self.rna;
+
         let x = fst.read();
         let y = snd.read();
         let z = thd.read();
-        let plain_codon = (x.as_ref().unwrap(), y.as_ref().unwrap(), z.as_ref().unwrap());
+
+        let plain_codon = (
+            x.as_ref().unwrap(),
+            y.as_ref().unwrap(),
+            z.as_ref().unwrap(),
+        );
+
         match plain_codon {
             // Reference: https://cnx.org/contents/GFy_h8cu@9.87:QEibhJMi@8/The-Genetic-Code
             // Square 1:
@@ -44,12 +51,84 @@ impl Codon {
             (&RNA::U, &RNA::U, &RNA::U) | (&RNA::U, &RNA::U, &RNA::C) => Amino::Phe,
 
             // Square 2:
-            // TODO: COMPLETE:
-            (&RNA::U, &RNA::C, &RNA::U) | (&RNA::U, &RNA::C, &RNA::C) => Amino::Ser,
+            (&RNA::U, &RNA::C, &RNA::U)
+            | (&RNA::U, &RNA::C, &RNA::C)
+            | (&RNA::U, &RNA::C, &RNA::A)
+            | (&RNA::U, &RNA::C, &RNA::G) => Amino::Ser,
 
-            // Used to compile.
-            // TODO: REMOVE:
-            _ => Amino::STOP
+            // Square 3:
+            (&RNA::U, &RNA::A, &RNA::U) | (&RNA::U, &RNA::A, &RNA::C) => Amino::Tyr,
+            (&RNA::U, &RNA::A, &RNA::A) | (&RNA::U, &RNA::A, &RNA::G) => Amino::STOP,
+
+            // Square 4:
+            (&RNA::U, &RNA::G, &RNA::U) | (&RNA::U, &RNA::G, &RNA::C) => Amino::Cys,
+            (&RNA::U, &RNA::G, &RNA::A) => Amino::STOP,
+            (&RNA::U, &RNA::G, &RNA::G) => Amino::Trp,
+
+            // Square 5:
+            (&RNA::C, &RNA::U, &RNA::U)
+            | (&RNA::C, &RNA::U, &RNA::C)
+            | (&RNA::C, &RNA::U, &RNA::A)
+            | (&RNA::C, &RNA::U, &RNA::G) => Amino::Leu,
+
+            // Square 6:
+            (&RNA::C, &RNA::C, &RNA::U)
+            | (&RNA::C, &RNA::C, &RNA::C)
+            | (&RNA::C, &RNA::C, &RNA::A)
+            | (&RNA::C, &RNA::C, &RNA::G) => Amino::Pro,
+
+            // Square 7:
+            (&RNA::C, &RNA::A, &RNA::U) | (&RNA::C, &RNA::A, &RNA::C) => Amino::His,
+            (&RNA::C, &RNA::A, &RNA::A) | (&RNA::C, &RNA::A, &RNA::G) => Amino::Gln,
+
+            // Square 8:
+            (&RNA::C, &RNA::G, &RNA::U)
+            | (&RNA::C, &RNA::G, &RNA::C)
+            | (&RNA::C, &RNA::G, &RNA::A)
+            | (&RNA::C, &RNA::G, &RNA::G) => Amino::Arg,
+
+            // Square 9:
+            (&RNA::A, &RNA::U, &RNA::U)
+            | (&RNA::A, &RNA::U, &RNA::C)
+            | (&RNA::A, &RNA::U, &RNA::A) => Amino::Ile,
+            // NOTE: Not start, assumed start has been dealt with
+            (&RNA::A, &RNA::U, &RNA::G) => Amino::Met,
+
+            // Square 10:
+            (&RNA::A, &RNA::C, &RNA::U)
+            | (&RNA::A, &RNA::C, &RNA::C)
+            | (&RNA::A, &RNA::C, &RNA::A)
+            | (&RNA::A, &RNA::C, &RNA::G) => Amino::Thr,
+
+            // Square 11:
+            (&RNA::A, &RNA::A, &RNA::U) | (&RNA::A, &RNA::A, &RNA::C) => Amino::Asn,
+            (&RNA::A, &RNA::A, &RNA::A) | (&RNA::A, &RNA::A, &RNA::G) => Amino::Lys,
+
+            // Square 12:
+            (&RNA::A, &RNA::G, &RNA::U) | (&RNA::A, &RNA::G, &RNA::C) => Amino::Ser,
+            (&RNA::A, &RNA::G, &RNA::A) | (&RNA::A, &RNA::G, &RNA::G) => Amino::Arg,
+
+            // Square 13:
+            (&RNA::G, &RNA::U, &RNA::U)
+            | (&RNA::G, &RNA::U, &RNA::C)
+            | (&RNA::G, &RNA::U, &RNA::A)
+            | (&RNA::G, &RNA::U, &RNA::G) => Amino::Val,
+
+            // Square 14:
+            (&RNA::G, &RNA::C, &RNA::U)
+            | (&RNA::G, &RNA::C, &RNA::C)
+            | (&RNA::G, &RNA::C, &RNA::A)
+            | (&RNA::G, &RNA::C, &RNA::G) => Amino::Ala,
+
+            // Square 15:
+            (&RNA::G, &RNA::A, &RNA::U) | (&RNA::G, &RNA::A, &RNA::C) => Amino::Asp,
+            (&RNA::G, &RNA::A, &RNA::A) | (&RNA::G, &RNA::A, &RNA::G) => Amino::Glu,
+
+            // Square 16:
+            (&RNA::G, &RNA::G, &RNA::U)
+            | (&RNA::G, &RNA::G, &RNA::C)
+            | (&RNA::G, &RNA::G, &RNA::A)
+            | (&RNA::G, &RNA::G, &RNA::G) => Amino::Gly,
         }
     }
 }
